a83e8e678ef9d9d3efdb4f1020be46ddfd32ad73
Julien Boyer <juliien.boyer49@orange.fr>
2018-02-01 09:12:32 +0100

    Add JWT generation

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Threading.Tasks;
 using events_planner.Services;
 using Microsoft.EntityFrameworkCore;
 using events_planner.Models;
+
+// JWT GENERATING ENGINE
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public class UserServices : IUserServices
     {
         public PlannerContext _context { get; set; }
+        public IConfiguration Configuration { get; set; }
 
-        public UserServices(PlannerContext context) {
+        public UserServices(PlannerContext context, IConfiguration configuration) {
             _context = context;
+            Configuration = configuration;
 
         }
 
@@ -18,8 +29,20 @@ namespace Microsoft.Extensions.DependencyInjection
             string userName = "George";
             string lastName = "Boulet";
 
+            var token = new JwtSecurityToken(
+                audience: Configuration["TokenAuthentication:Audience"],
+                issuer: Configuration["TokenAuthentication:Issuer"],
+                claims: new Claim[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, userName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                },
+                expires: DateTime.UtcNow.AddDays(2),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenAuthentication:Secret"])), SecurityAlgorithms.HmacSha256)
+            );
+
             return new {
-                Token = "123456789" + userName + lastName
+                Token = new JwtSecurityTokenHandler().WriteToken(token) 
             };
         }
     }
