25448c641530b60f7140bccdfacc427cc7769c50
julien boyer <juh@MacBook-Pro-de-julien.local>
2018-02-20 00:04:16 +0100

    Add Read Integration Tests

--- PlannerApi.Tests/IntegrationTests/UserControllerTests.cs
+++ PlannerApi.Tests/IntegrationTests/UserControllerTests.cs
@@ -30,6 +30,8 @@ namespace PlannerApi.Tests.IntegrationTests
                 HttpResponseMessage response = await HttpClient.PostAsync("api/user/token", new StringContent(fakeJson, System.Text.Encoding.UTF8, "application/json"));
                 string token = await response.Content.ReadAsStringAsync();
 
+                Console.WriteLine(token);
+
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 Assert.NotNull(token);
             }
@@ -61,6 +63,20 @@ namespace PlannerApi.Tests.IntegrationTests
                 Assert.NotNull(obj);
                 Assert.Equal(obj.Email, user.Email);
                 Assert.Equal(obj.Role.Name, "Student");
+            }
+        }
+
+        public partial class ReadUser : UserControllerTests {
+            public ReadUser(ServerFixtures server) : base(server) { }
+
+            [Fact]
+            public async Task ShouldReturnTheUser() {
+                string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKdWxpZW4iLCJnaXZlbl9uYW1lIjoiQm95ZXIiLCJlbWFpbCI6Imp1bGllbkBnbWFpbC5jb20iLCJqdGkiOiI3NmNkNDE0ZC1kZDgyLTQxODktOTA5MS02NTg0YmRiNzQ1ZjAiLCJuYmYiOjE1MTkwODA3MjMsImV4cCI6MTUxOTI1MzUyMywiaXNzIjoibG9jYWxob3N0OjUwMDAiLCJhdWQiOiJsb2NhbGhvc3Q6NTAwMCJ9.cO8Tsb_uZc-3un06FbodxqhdzSrBKUHquaCRKY3eyaU";
+
+                HttpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await HttpClient.GetAsync("api/user", HttpCompletionOption.ResponseContentRead);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             }
         }
     }
