408cb3484b571547187dbb8251ff635a4229f3ad
julien boyer <juh@MacBook-Pro-de-julien.local>
2018-02-19 20:13:18 +0100

    Enhance Server Fixture

--- PlannerApi.Tests/Fixtures/ServerFixture.cs
+++ PlannerApi.Tests/Fixtures/ServerFixture.cs
@@ -4,6 +4,10 @@ using System.IO;
 using events_planner;  
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using events_planner.Models;
+using events_planner.Data;
+using Microsoft.Extensions.Logging;
 
 namespace PlannerApi.Tests.Fixtures {
 
@@ -13,9 +17,28 @@ namespace PlannerApi.Tests.Fixtures {
 
         public ServerFixtures() {
             IWebHostBuilder builder = Program.CreateWebHostbuilder(Array.Empty<string>())
+                                             .UseEnvironment("test")
                                              .UseContentRoot(Path.GetFullPath("../../../../PlannerApi"));
 
             TestServer Server = new TestServer(builder);
+
+            using (var scope = Server.Host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                try
+                {
+                    DbSeeder.InitializeTest(
+                        services.GetRequiredService<PlannerContext>()
+                    );
+                }
+                catch (Exception ex)
+                {
+                    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+
             Client = Server.CreateClient();
         }
 
