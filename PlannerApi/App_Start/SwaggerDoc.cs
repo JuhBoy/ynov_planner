dfeefeb972b4e9281f57cb0d520818c5cc139894
julien boyer <juh@MacBook-Pro-de-julien.local>
2018-03-14 18:25:31 +0100

    Add jwt Authorization Bearer for swagger api

--- PlannerApi/App_Start/SwaggerDoc.cs
+++ PlannerApi/App_Start/SwaggerDoc.cs
@@ -25,7 +25,15 @@ namespace events_planner
                 var basePath = AppContext.BaseDirectory;
                 var xmlPath = Path.Combine(basePath, "events_planner.xml"); 
                 c.IncludeXmlComments(xmlPath);
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
             }); 
+
         }
 
         private void swaggerConfigure(IApplicationBuilder application) {
