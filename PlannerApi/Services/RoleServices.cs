6dee4679d7e3e32fc53e4aa696a5cd4338ec78c0
julien boyer <juh@MacBook-Pro-de-julien.local>
2018-03-21 18:06:46 +0100

    Update formatting

--- PlannerApi/Services/RoleServices.cs
+++ PlannerApi/Services/RoleServices.cs
@@ -3,21 +3,18 @@ using events_planner.Models;
 using events_planner.Services;
 
 namespace Microsoft.Extensions.DependencyInjection {
-    public class RoleServices : IRoleServices
-    {
+    public class RoleServices : IRoleServices {
         public PlannerContext Context { get; set; }
 
         public RoleServices(PlannerContext context) {
             Context = context;
         }
 
-        Role IRoleServices.GetRoleByName(string name)
-        {
+        Role IRoleServices.GetRoleByName(string name) {
             return Context.Role.FirstOrDefault((Role arg) => arg.Name == name);
         }
 
-        Role IRoleServices.GetStudentRole()
-        {
+        Role IRoleServices.GetStudentRole() {
             return Context.Role.FirstOrDefault((Role arg) => arg.Name == "Student");
         }
     }
