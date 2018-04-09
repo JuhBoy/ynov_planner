24a2ad42559912da910393ed453ad2588ba0c617
julien boyer <juh@macbook-pro-de-julien.home>
2018-04-09 22:24:15 +0200

    Booking unsubscribe to Event

--- PlannerApi/Controllers/BookingController.cs
+++ PlannerApi/Controllers/BookingController.cs
@@ -1,10 +1,8 @@
-
+using events_planner.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Authorization;
-using System.Security.Claims;
 using events_planner.Models;
 using System.Linq;
 
@@ -13,15 +11,17 @@ namespace events_planner.Controllers {
     [Route("api/[controller]"), Authorize(Roles = "Student,Admin")]
     public class BookingController : BaseController {
 
-        public BookingController(PlannerContext context) {
+        public IEventServices EventServices;
+
+        public BookingController(PlannerContext context,
+                                IEventServices eventServices) {
             Context = context;
+            EventServices = eventServices;
         }
 
         [HttpGet("subscribe/{eventId}")]
         public async Task<IActionResult> Subscribe(int eventId) {
-            Event @event = await Context.Event
-                                        .AsTracking()
-                                        .FirstOrDefaultAsync((Event ev) => ev.Id == eventId);
+            Event @event = await EventServices.GetEventByIdAsync(eventId);
 
             if (@event == null || @event.SubscribedNumber >= @event.SubscribeNumber)
                 return BadRequest("Unprocessable Operation");
@@ -41,6 +41,30 @@ namespace events_planner.Controllers {
             try {
                 Context.Booking.Add(book);
                 Context.Event.Update(@event);
+                await Context.SaveChangesAsync();
+            } catch (DbUpdateException e) {
+                return BadRequest(e.InnerException.Message);
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet("unsubscribe/{eventId}")]
+        public async Task<IActionResult> UnSubscribe(int eventId) {
+            Event @event = await EventServices.GetEventByIdAsync(eventId);
+            Booking booking = await Context.Booking.FirstOrDefaultAsync(
+                (Booking book) => book.UserId == CurrentUser.Id && 
+                                  book.EventId == eventId
+            );
+
+            if (@event == null || booking == null || @event.Expired())
+                return BadRequest("Event not found or Expired");
+
+            @event.SubscribedNumber--;
+
+            try {
+                Context.Update(@event);
+                Context.Remove(booking);
                 await Context.SaveChangesAsync();
             } catch (DbUpdateException e) {
                 return BadRequest(e.InnerException.Message);
