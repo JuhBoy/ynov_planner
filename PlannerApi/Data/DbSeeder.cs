10443efaf075379c9b062e0877b90e57c1bd7709
Julien Boyer <julien.boyer@mapotempo.com>
2018-08-31 10:32:34 +0200

    Improvement: Add testing fake account for beta

--- PlannerApi/Data/DbSeeder.cs
+++ PlannerApi/Data/DbSeeder.cs
@@ -15,8 +15,9 @@ namespace events_planner.Data {
 
         public const string PROMOTIONS_PATH = "/Data/Promotions.json";
         public const string ROLE_PARH = "/Data/Roles.json";
+        public const string USERS_PATH = "/Data/Users.json";
 
-        public const string USERS_TEST_PARTH = "/Fixtures/UsersTest.json";
+        public const string USERS_TEST_PATH = "/Fixtures/UsersTest.json";
         public const string PROMOTIONS_TEST_PATH = "/Fixtures/Promotions.json";
         public const string ROLE_TEST_PATH = "/Fixtures/Roles.json";
         public const string EVENTS_TEST_PATH = "/Fixtures/Events.json";
@@ -28,16 +29,29 @@ namespace events_planner.Data {
                 context.Database.Migrate();
 
                 string currentDirectory = Directory.GetCurrentDirectory();
+                List<Promotion> promotions = JsonConvert.DeserializeObject<List<Promotion>>(File.ReadAllText(currentDirectory + PROMOTIONS_PATH));
+                List<Role> roles = JsonConvert.DeserializeObject<List<Role>>(File.ReadAllText(currentDirectory + ROLE_PARH));
 
-                if (!context.Promotion.Any())
-                {
-                    List<Promotion> promotions = JsonConvert.DeserializeObject<List<Promotion>>(File.ReadAllText(currentDirectory + PROMOTIONS_PATH));
+                if (!context.Promotion.Any()) {
                     context.Promotion.AddRange(promotions);
                 }
 
                 if (!context.Role.Any()) {
-                    List<Role> roles = JsonConvert.DeserializeObject<List<Role>>(File.ReadAllText(currentDirectory + ROLE_PARH));
                     context.Role.AddRange(roles);
+                }
+
+                if (!context.User.Any()) {
+                    List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(currentDirectory + USERS_PATH));
+                    foreach (var user in users) {
+                        if (user.Email.Contains("admin")) {
+                            user.Role = roles.FirstOrDefault(e => e.Name.Equals("Admin"));
+                            user.Promotion = promotions.FirstOrDefault(e => e.Name == "STAFF");
+                        } else {
+                            user.Role = roles.FirstOrDefault(e => e.Name.Equals("Student"));
+                            user.Promotion = promotions.FirstOrDefault(e => e.Name == "YNOV-Student");
+                        }
+                    }
+                    context.User.AddRange(users);
                 }
 
                 if (ENV == "Development" && !context.Event.Any()) {
@@ -58,7 +72,7 @@ namespace events_planner.Data {
             if (!context.User.Any()) {
                 List<Promotion> promotions = JsonConvert.DeserializeObject<List<Promotion>>(File.ReadAllText(currentDirectory + PROMOTIONS_TEST_PATH));
                 List<Role> roles = JsonConvert.DeserializeObject<List<Role>>(File.ReadAllText(currentDirectory + ROLE_TEST_PATH));
-                List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(currentDirectory + USERS_TEST_PARTH));
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(currentDirectory + USERS_TEST_PATH));
                 List<Event> events = JsonConvert.DeserializeObject<List<Event>>(File.ReadAllText(currentDirectory + EVENTS_TEST_PATH));
                 
                 context.Event.AddRange(events);
